6640544d06cf8bcdfe8eb3b7fb4d436a1258ccc6
gpwi970725 <gpwi970725@gmail.com>
2020-12-28 01:58:07 +0900

    회원가입 정보 받아오기

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const express = require('express')
 const app = express()
 /* 5000번 포트를 백서버로 둔다 */
 const port = 5000
+/* body-parser를 가져온다 */
+const bodyParser = require('body-parser');
+/* User model을 가져온다 */
+const { User } = require("./models/User");
+
+
+/* body-parser의 옵션을 준다 */
+// application/x-www-form-urlencoded 로 된 데이터를 분석해서 가져올 수 있도록
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json type의 데이터를 분석해서 가져올 수 있도록
+app.use(bodyParser.json());
 
 
 /* mongoose module을 가져온다 */
@@ -19,6 +30,31 @@ mongoose.connect('mongodb+srv://hyejikim:1234@boilerplate.zebjf.mongodb.net/<dbn
 /* root directory에 오면 Hello World! 출력
    브라우저 주소에 localhost:5000/ 입력하면 확인가능 */
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+/* 회원가입을 위한 Register Route 만들기
+   Route의 Endpoint : /register
+   callback function : request, response */
+app.post('/register', (req, res) => {
+   // 회원가입할 때 필요한 정보들을 client에서 가져오면
+   // 그것들을 데이터베이스에 넣어준다.
+
+   /* req.body 에는 json 형식으로 데이터들이 들어있는데
+      이 데이터들은 body-parser가 client의 정보를 받아준 것  */
+   const user = new User(req.body)
+
+   /* save() : mongoDB에서 오는 method, 정보들이 user model에 저장됨
+      err, userInfo : callback function */
+   user.save((err, userInfo) => {
+      /* 저장할 때 에러가 있으면 client에 에러 있다고 json 형식으로(에러 메시지 포함) 전달 */
+      if(err) return res.json({ success: false, err})
+      /* status(200)는 성공 의미 */
+      return res.status(200).json({
+         success: true
+      })
+   } )
+})
+
 /* port 5000번에서 app 실행
    app이 5000에 listen을 하면 console print */
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
