77fde8030543664ea31f28be047857ab498e3329
gpwi970725 <gpwi970725@gmail.com>
2021-01-03 17:34:26 +0900

    token 생성

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const config = require('./config/key')
 
 /* body-parser의 옵션을 준다 */
 // application/x-www-form-urlencoded 로 된 데이터를 분석해서 가져올 수 있도록
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // application/json type의 데이터를 분석해서 가져올 수 있도록
 app.use(bodyParser.json());
 
@@ -26,7 +26,7 @@ mongoose.connect(config.mongoURI, {
    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
    /* MongoDB 연결 성공/실패 시 동작 */
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+   .catch(err => console.log(err))
 
 
 /* root directory에 오면 Hello World! 출력
@@ -49,12 +49,12 @@ app.post('/register', (req, res) => {
       err, userInfo : callback function */
    user.save((err, userInfo) => {
       /* 저장할 때 에러가 있으면 client에 에러 있다고 json 형식으로(에러 메시지 포함) 전달 */
-      if(err) return res.json({ success: false, err})
+      if (err) return res.json({ success: false, err })
       /* status(200)는 성공 의미 */
       return res.status(200).json({
          success: true
       })
-   } )
+   })
 })
 
 /* 로그인을 위한 Login Route 만들기
@@ -64,7 +64,7 @@ app.post('/login', (req, res) => {
 
    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
    User.findOne({ email: req.body.email }, (err, user) => {  /* User model을 가져와서 mongoDB에서 제공하는 findOne 메소드를 이용해 요청한 이메일이 DB에 있는지 찾는다 */
-      if(!user) {  /* 만약 이메일이 DB에 없다면 user는 비어있을 것 */
+      if (!user) {  /* 만약 이메일이 DB에 없다면 user는 비어있을 것 */
          return res.json({
             loginSuccess: false,
             message: "제공된 이메일에 해당하는 유저가 없습니다."
@@ -72,13 +72,16 @@ app.post('/login', (req, res) => {
       }
 
       // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
-      user.comparePassword(req.body.password , (err, isMatch) => {  /* user model에 만든 comparePassword 메소드 사용, req.body.password 는 plainPassword(입력한 그대로의 PW) */
+      user.comparePassword(req.body.password, (err, isMatch) => {  /* user model에 만든 comparePassword 메소드 사용, req.body.password 는 plainPassword(입력한 그대로의 PW) */
          /* isMatch가 없으면 비밀번호가 틀림 */
-         if(!isMatch)
+         if (!isMatch)
             return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
-         
+
          // 비밀번호까지 맞다면 그 유저를 위한 토큰을 생성하기
          user.gernerateToken((err, user) => {  /* user model에 만든 gernerateToken 메소드 사용 */
+            if (err) return res.status(400).send(err);
+
+            // user에 들어있는 토큰을 저장한다. 쿠키 / 로컬스토리지 / 세션 에 저장 가능 => 지금은 쿠키에 저장
             
          })
       })
